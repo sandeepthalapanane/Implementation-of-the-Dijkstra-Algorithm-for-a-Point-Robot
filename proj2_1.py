5345837bafb80bf41a16ae572010d85cd4f05ba8
SANDEEP THALAPANANE <ssandy086@gmail.com>
2023-03-11 17:54:43 -0500

    added visualization

--- proj2_1.py
+++ proj2_1.py
@@ -1,228 +1,201 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Polygon
 from queue import PriorityQueue
+import time
+import pygame
+import vidmaker
 
-fig = plt.figure()
-fig.set_dpi(100)
-fig.set_size_inches(24, 10)
-ax = plt.axes(xlim=(0, 600), ylim=(0, 250))
-
-j = 0
+st = time.time()
 
 
-def create_map():
-    obstacles = []
-    x_1 = []
-    y_1 = []
-    for x in range(0, 601, 1):
-        for y in range(0, 251, 1):
-            if (x >= 230 and x <= 370 and (y-((15/26)*x) - 32.695) <= 0
-                and (y+((15/26)*x) - 378.849) <= 0 and (y+((15/26)*x) - 217.304) >= 0
-                    and (y-((15/26)*x) + 128.849) >= 0):
-                obstacles.append((int(x), int(y)))
-                x_1.append(int(x))
-                y_1.append(int(y))
-            if (x >= 95 and y >= 0 and x <= 155 and y <= 105):
-                obstacles.append((int(x), int(y)))
-                x_1.append(int(x))
-                y_1.append(int(y))
-            if (x >= 95 and y >= 145 and x <= 155 and y <= 250):
-                obstacles.append((int(x), int(y)))
-                x_1.append(int(x))
-                y_1.append(int(y))
-            if (x >= 455 and (y+(2*x) - 1156.18) <= 0 and (y-2*x + 906.18) >= 0):
-                obstacles.append((int(x), int(y)))
-                x_1.append(int(x))
-                y_1.append(int(y))
-    ax.scatter(x_1, y_1, color='r', marker='o')
-    return obstacles
+def to_pygame(coords, height):
+    return (coords[0], height - coords[1])
 
 
-def input_start(obstacles, str):
-    print("Enter", str, "node (Sample: 10, 10 ): ")
-    A = [int(i) for i in input().split(', ')]
-    A_1 = (A[0], A[1])
-    if A_1 in obstacles:
-        print("The entered input lies on the obstacles, please try again")
-        input_start(obstacles, str)
-    return A_1
+def rect_pygame(coords, height, obj_height):
+    return (coords[0], height - coords[1] - obj_height)
+
+
+def create_map(visit, backtrack, start, Goal):
+    pygame.init()
+    size = [600, 250]
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Visualization")
+    video = vidmaker.Video("vidmaker.mp4", late_export=True)
+
+    done = False
+    clock = pygame.time.Clock()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        screen.fill("white")
+        x, y = rect_pygame([95, 0], 250, 105)
+        pygame.draw.rect(screen, "blue", [x, y, 60, 105], 0)
+        x, y = rect_pygame([100, 0], 250, 100)
+        pygame.draw.rect(screen, "red", [x, y, 50, 100], 0)
+        x, y = rect_pygame([95, 145], 250, 105)
+        pygame.draw.rect(screen, "blue", [x, y, 60, 105], 0)
+        a, b = rect_pygame([100, 150], 250, 100)
+        pygame.draw.rect(screen, "red", [a, b, 50, 100], 0)
+        pygame.draw.rect(screen, "blue", [0, 0, 5, 250], 0)
+        pygame.draw.rect(screen, "blue", [0, 0, 600, 5], 0)
+        pygame.draw.rect(screen, "blue", [0, 245, 600, 5], 0)
+        pygame.draw.rect(screen, "blue", [595, 0, 5, 250], 0)
+        a, b = to_pygame([455, 20], 250)
+        c, d = to_pygame([463, 20], 250)
+        e, f = to_pygame([1031/2, 125], 250)
+        g, h = to_pygame([463, 230], 250)
+        i, j = to_pygame([455, 230], 250)
+        pygame.draw.polygon(
+            screen, "blue", ([a, b], [c, d], [e, f], [g, h], [i, j]), 0)
+        a, b = to_pygame([460, 25], 250)
+        c, d = to_pygame([460, 225], 250)
+        e, f = to_pygame([510, 125], 250)
+        pygame.draw.polygon(screen, "red", [[a, b], [c, d], [e, f]], 0)
+        a, b = to_pygame([300, 2675/13], 250)
+        c, d = to_pygame([230, 2150/13], 250)
+        e, f = to_pygame([230, 1100/13], 250)
+        g, h = to_pygame([300, 575/13], 250)
+        i, j = to_pygame([370, 1100/13], 250)
+        k, l = to_pygame([370, 2150/13], 250)
+        pygame.draw.polygon(screen, "blue", [[a, b], [c, d], [
+                            e, f], [g, h], [i, j], [k, l]], 0)
+        pygame.draw.polygon(screen, "red", ((235, 87.5), (300, 50),
+                            (365, 87.5), (365, 162.5), (300, 200), (235, 162.5)))
+        pygame.draw.circle(screen, (255, 255, 0), to_pygame(start, 250), 1)
+        pygame.draw.circle(screen, (255, 255, 0), to_pygame(Goal, 250), 1)
+        for j in visit:
+            pygame.draw.circle(screen, (50, 137, 131), to_pygame(j, 250), 1)
+            video.update(pygame.surfarray.pixels3d(
+                screen).swapaxes(0, 1), inverted=False)
+            pygame.display.flip()
+            clock.tick(400)
+        pygame.draw.circle(screen, (255, 255, 0), to_pygame(start, 250), 1)
+        pygame.draw.circle(screen, (255, 255, 0), to_pygame(Goal, 250), 1)
+        for i in backtrack:
+            pygame.draw.circle(screen, (255, 255, 0), to_pygame(i, 250), 1)
+            video.update(pygame.surfarray.pixels3d(
+                screen).swapaxes(0, 1), inverted=False)
+            pygame.display.flip()
+            clock.tick(400)
+        pygame.draw.circle(screen, (255, 255, 0), to_pygame(start, 250), 1)
+        pygame.draw.circle(screen, (255, 255, 0), to_pygame(Goal, 250), 1)
+        pygame.display.flip()
+        pygame.time.wait(3000)
+        done = True
+    pygame.quit()
+    video.export(verbose=True)
+
+
+def check_obstacles(coordinates):
+    x, y = coordinates[0], coordinates[1]
+    if (x >= 230 and x <= 370 and (y-((15/26)*x) - 32.695) <= 0
+        and (y+((15/26)*x) - 378.849) <= 0 and (y+((15/26)*x) - 217.304) >= 0
+            and (y-((15/26)*x) + 128.849) >= 0):
+        return False
+    if (x >= 95 and y >= 0 and x <= 155 and y <= 105):
+        return False
+    if (x >= 95 and y >= 145 and x <= 155 and y <= 250):
+        return False
+    if (x >= 595 or y >= 245 or x <= 5 or y <= 5):
+        return False
+    if (x >= 455 and (y+(2*x) - 1156.18) <= 0 and y <= 230 and y >= 20 and (y-2*x + 906.18) >= 0):
+        return False
+    if (x < 0 or y < 0):
+        return False
+    if (x > 600 or y > 250):
+        return False
+    return True
+
+
+def input_start(str):
+    while True:
+        print("Enter", str, "node (Sample: 10, 10 ): ")
+        A = [int(i) for i in input().split(', ')]
+        A_1 = (A[0], A[1])
+        if not check_obstacles(A_1):
+            print(
+                "The entered input lies on the obstacles (or) not valid, please try again")
+        else:
+            return A_1
+
+
+def check_conditions(que, coordinates, cost_to_come):
+    if coordinates in touch:
+        if touch[coordinates] > cost_to_come:
+            new_que = (cost_to_come, coordinates)
+            Path[coordinates] = que[1]
+            Q.put(new_que)
+            touch[coordinates] = cost_to_come
+            return
+        else:
+            return
+    new_que = (cost_to_come, coordinates)
+    Path[coordinates] = que[1]
+    Q.put(new_que)
+    touch[coordinates] = cost_to_come
 
 
 def moveup(que):
-    global j
-    coordinates = (que[3][0], que[3][1]+1)
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0], que[1][1]+1)
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def movedown(que):
-    global j
-    coordinates = (que[3][0], que[3][1]-1)
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0], que[1][1]-1)
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def moveleft(que):
-    global j
-    coordinates = (que[3][0]-1, que[3][1])
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0]-1, que[1][1])
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def moveright(que):
-    global j
-    coordinates = (que[3][0]+1, que[3][1])
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0]+1, que[1][1])
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def moveupleft(que):
-    global j
-    coordinates = (que[3][0]-1, que[3][1]+1)
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0]-1, que[1][1]+1)
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1.4
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def moveupright(que):
-    global j
-    coordinates = (que[3][0]+1, que[3][1]+1)
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0]+1, que[1][1]+1)
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1.4
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def movedownleft(que):
-    global j
-    coordinates = (que[3][0]-1, que[3][1]-1)
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0]-1, que[1][1]-1)
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1.4
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def movedownright(que):
-    global j
-    coordinates = (que[3][0]+1, que[3][1]-1)
-    if coordinates not in visit and coordinates not in obstacle:
+    coordinates = (que[1][0]+1, que[1][1]-1)
+    if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1.4
-        for i in range(Q.qsize()):
-            if Q.queue[i][3] == coordinates:
-                if Q.queue[i][0] < cost_to_come:
-                    return
-                else:
-                    j += 1
-                    Q.queue[i] = (cost_to_come, j, que[1], coordinates)
-                    Path[coordinates] = que[3]
-                    return
-        parent_index = que[1]
-        j += 1
-        new_que = (cost_to_come, j, parent_index, coordinates)
-        Path[coordinates] = que[3]
-        Q.put(new_que)
+        check_conditions(que, coordinates, cost_to_come)
 
 
 def generate_path(path, start, Goal):
     backtrack = []
-    key = path.get(goal)
-    backtrack.append((Goal))
-    backtrack.append((key))
+    key = path.get(Goal)
+    backtrack.append(Goal)
+    backtrack.append(key)
     while (key != start):
         key = path.get(key)
         backtrack.append(key)
@@ -230,48 +203,43 @@ def generate_path(path, start, Goal):
     return backtrack
 
 
-obstacle = create_map()
-Start = input_start(obstacle, 'Start')
-goal = input_start(obstacle, 'Goal')
-# Start, goal = (0, 0), (50, 50)
+Start = input_start('Start')
+goal = input_start('Goal')
 print(Start, goal)
 visit = []
+touch = {}
 Path = {}
 Q = PriorityQueue()
-Q.put((0, 0, 0, (Start)))
+Q.put((0, Start))
 while (Q.qsize() != 0):
     queue = Q.get()
-    visit.append(queue[3])
-    if (queue[3] != goal):
-        if (queue[3][1]+1 >= 0 and queue[3][1]+1 <= 250):
-            moveup(queue)
-        if (queue[3][1]-1 >= 0 and queue[3][1]-1 <= 250):
-            movedown(queue)
-        if (queue[3][0]-1 >= 0 and queue[3][0]-1 <= 600):
-            moveleft(queue)
-        if (queue[3][0]+1 >= 0 and queue[3][0]+1 <= 600):
-            moveright(queue)
-        if (queue[3][1]+1 <= 250 and queue[3][0]-1 >= 0):
-            moveupleft(queue)
-        if (queue[3][0]+1 <= 600 and queue[3][1]+1 <= 250):
-            moveupright(queue)
-        if (queue[3][0]-1 >= 0 and queue[3][1]-1 >= 0):
-            movedownleft(queue)
-        if (queue[3][0]+1 <= 600 and queue[3][1]-1 <= 250):
-            movedownright(queue)
+    if (queue[1] != goal):
+        if queue[1] not in visit:
+            visit.append(queue[1])
+            if (queue[1][1]+1 >= 0 and queue[1][1]+1 <= 250):
+                moveup(queue)
+            if (queue[1][1]-1 >= 0 and queue[1][1]-1 <= 250):
+                movedown(queue)
+            if (queue[1][0]-1 >= 0 and queue[1][0]-1 <= 600):
+                moveleft(queue)
+            if (queue[1][0]+1 >= 0 and queue[1][0]+1 <= 600):
+                moveright(queue)
+            if (queue[1][1]+1 <= 250 and queue[1][0]-1 >= 0):
+                moveupleft(queue)
+            if (queue[1][0]+1 <= 600 and queue[1][1]+1 <= 250):
+                moveupright(queue)
+            if (queue[1][0]-1 >= 0 and queue[1][1]-1 >= 0):
+                movedownleft(queue)
+            if (queue[1][0]+1 <= 600 and queue[1][1]-1 <= 250):
+                movedownright(queue)
     else:
         print('success')
         Backtrack = generate_path(Path, Start, goal)
         print(Backtrack)
         print('-----------')
         print(queue)
+        et = time.time()
+        elapsed_time = et - st
+        print('Time to calculate path:', elapsed_time, 'seconds')
+        create_map(visit, Backtrack, Start, goal)
         break
-
-# for ele in visit:
-#     ax.scatter(ele[0], ele[1], marker='s', color='#1f77b4')
-#     plt.pause(0.025)
-
-# for i in Backtrack:
-#     ax.scatter(i[0], i[1], marker='s', color='k')
-#     plt.pause(0.025)
-# plt.show()
